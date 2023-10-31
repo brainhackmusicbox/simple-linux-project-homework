389bde1e556d9bf5b3996a364a61cf0ff7935cd8
Stanislav Bobrytskyi <beavered@gmail.com>
2023-10-31 18:40:23 +0200

    Simple C project demonstrating the usage of autoconf/automake to build and install/uninstall project
    Signed-off-by: Stanislav Bobrytskyi <beavered@gmail.com>

---
+++ func.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "header.h"
+
+
+void printfFunc(void) {
+	printf("This is the mesage from printfHelloMake() in func.c !\n");
+	return;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "header.h"
+
+int main() {
+	printf("This is the message from main()!\n");
+	
+	printfFunc();
+	return 0;
+}
